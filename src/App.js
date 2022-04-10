5a10a1b4223173f605f0478aa7482c5045547db2
Omer-Cicek <omercicek160408@gmail.com>
2022-04-10 03:46:15 +0300

    details page update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+//react router
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//imported pages to used for router
 import Navbar from './components/navbar/Navbar';
 import Home from './pages/home/Home';
 import About from './pages/about/About';
 import Login from './pages/login/Login';
 import Details from './pages/details/Details';
 
+//component
 function App() {
   return (
     <div className="App">
